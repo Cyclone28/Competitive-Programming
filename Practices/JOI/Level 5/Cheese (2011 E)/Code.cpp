1af2d376fc3bc4095e4b1806ba977deff9216cd4
Cyclone28 <nohelper2828@gmail.com>
2020-11-26 16:39:40 +0900

    JOI

---
+++ Practices/JOI/Level 5/Cheese (2011 E)/Code.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bfs(int p, int q){
+    
+}
+
+int main(){
+    int h, w, n;
+    cin >> h >> w >> n;
+    int p, q;
+    char a[h+1][w+1] = {};
+    for(int i = 0; i < h; i++){
+        for(int j = 0; j < w; j++){
+            cin >> a[i][j];
+            p = i, q = j;
+        }
+    }
+
+}
